691970dcf0b141a3169db95c855cb4babbabd49a
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2021-03-06 23:02:22 +0000

    Added typescript, unit test, routing and vuex support

---
+++ AJBellTech/clientapp/src/components/Ticker.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Ticker",
+
+  data() {
+    return {};
+  },
+
+  methods: {}
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ AJBellTech/clientapp/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>AJ Bell Investment Platform</h1>
+    <Ticker></Ticker>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import Ticker from "@/components/Ticker.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: { Ticker },
+});
+</script>
