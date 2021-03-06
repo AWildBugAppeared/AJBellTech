f9adff7e9c5e1172131f2fc9b23cc2f5f229e598
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2021-03-06 13:07:42 +0000

    Add Project Files

---
+++ AJBellTech/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AJBellTech.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]/{id?}")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
